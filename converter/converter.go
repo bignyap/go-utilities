4bb9ed4a36dbb3343626490510bd32d208653715
bpathi <bpathi@IN-BPATHI-M.local>
2025-06-25 21:35:42 +0530

    adding some convertor and formvalidator functions. tbh, we might not use them often and probably we do not need them as there are many library readily available doing that

---
+++ converter/converter.go
@@ -0,0 +1,205 @@
+package converter
+
+import (
+	"database/sql"
+	"time"
+)
+
+func StrTo[T any](str string, converter Converter[T]) (T, error) {
+	return ConvertString(str, converter)
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//							Str to DataTypes -- Helpful with API form validation
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+func StrToInt(str string) (int, error) {
+	return StrTo[int](str, IntConverter{})
+}
+
+func StrToFloat(str string) (float64, error) {
+	return StrTo[float64](str, FloatConverter{})
+}
+
+func StrToBool(str string) (bool, error) {
+	return StrTo[bool](str, BoolConverter{})
+}
+
+func StrToDate(str string) (time.Time, error) {
+	return StrTo[time.Time](str, DateConverter{})
+}
+
+func StrToUnixTime(str string) (int, error) {
+	return StrTo[int](str, UnixTimeConverter{})
+}
+
+func StrToNullInt32(str string) (sql.NullInt32, error) {
+	return StrTo[sql.NullInt32](str, NullInt32Converter{})
+}
+
+func StrToNullInt64(str string) (sql.NullInt64, error) {
+	return StrTo[sql.NullInt64](str, NullInt64Converter{})
+}
+
+func StrToNullFloat64(str string) (sql.NullFloat64, error) {
+	return StrTo[sql.NullFloat64](str, NullFloat64Converter{})
+}
+
+func StrToNullBool(str string) (sql.NullBool, error) {
+	return StrTo[sql.NullBool](str, NullBoolConverter{})
+}
+
+func StrToNullStr(str string) sql.NullString {
+	res, _ := StrTo[sql.NullString](str, NullStringConverter{})
+	return res
+}
+
+func StrToNullTime(str string) (sql.NullTime, error) {
+	return StrTo[sql.NullTime](str, NullTimeConverter{})
+}
+
+func StrToUnixNullTime(str string) (sql.NullInt64, error) {
+	return StrTo[sql.NullInt64](str, NullUnixTime64Converter{})
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//						sql.Null to Type Specific Converter -- Helpful in SQL to Go Type
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+type Nullable interface {
+	IsValid() bool
+}
+
+type MyNullString struct {
+	sql.NullString
+}
+
+type MyNullBool struct {
+	sql.NullBool
+}
+
+type MyNullInt32 struct {
+	sql.NullInt32
+}
+
+type MyNullInt64 struct {
+	sql.NullInt64
+}
+
+type MyNullTime struct {
+	sql.NullTime
+}
+
+func (n MyNullString) IsValid() bool {
+	return n.Valid
+}
+
+func (n MyNullBool) IsValid() bool {
+	return n.Valid
+}
+
+func (n MyNullInt32) IsValid() bool {
+	return n.Valid
+}
+
+func (n MyNullInt64) IsValid() bool {
+	return n.Valid
+}
+
+func (n MyNullTime) IsValid() bool {
+	return n.Valid
+}
+
+func NullToPointer[T Nullable, P any](input *T, converter func(*T) P) *P {
+	if input == nil || !(*input).IsValid() {
+		return nil
+	}
+	val := converter(input)
+	return &val
+}
+
+func FromNullString(input *MyNullString) string {
+	return input.String
+}
+
+func FromNullBool(input *MyNullBool) bool {
+	return input.Bool
+}
+
+func FromNullInt32(input *MyNullInt32) int {
+	return int(input.Int32)
+}
+
+func FromNullInt64(input *MyNullInt64) int {
+	return int(input.Int64)
+}
+
+func FromNullTime(input *MyNullTime) time.Time {
+	return time.Time(input.Time)
+}
+
+func FromNullInt32ToTime(input *MyNullInt32) time.Time {
+	return misc.FromUnixTime32(input.Int32)
+}
+
+func NullStrToStr(input *sql.NullString) *string {
+	val := MyNullString{NullString: *input}
+	return NullToPointer(&val, FromNullString)
+}
+
+func NullBoolToBool(input *sql.NullBool) *bool {
+	val := MyNullBool{NullBool: *input}
+	return NullToPointer(&val, FromNullBool)
+}
+
+func NullInt32ToInt(input *sql.NullInt32) *int {
+	val := MyNullInt32{NullInt32: *input}
+	return NullToPointer(&val, FromNullInt32)
+}
+
+func NullInt64ToInt(input *sql.NullInt64) *int {
+	val := MyNullInt64{NullInt64: *input}
+	return NullToPointer(&val, FromNullInt64)
+}
+
+func NullTimeToTime(input *sql.NullTime) *time.Time {
+	val := MyNullTime{NullTime: *input}
+	return NullToPointer(&val, FromNullTime)
+}
+
+func NullInt32ToTime(input *sql.NullInt32) *time.Time {
+	val := MyNullInt32{NullInt32: *input}
+	return NullToPointer(&val, FromNullInt32ToTime)
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//										Pointer to SQLNull Types
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+func IntPtrToNullInt32(ptr *int) sql.NullInt32 {
+	if ptr != nil {
+		return sql.NullInt32{Int32: int32(*ptr), Valid: true}
+	}
+	return sql.NullInt32{Valid: false}
+}
+
+func TimePtrToUnixInt(t *time.Time) *int {
+	if t == nil {
+		return nil
+	}
+	unixTime := int(t.Unix())
+	return &unixTime
+}
+
+func BoolPtrToNullBool(b *bool) sql.NullBool {
+	if b != nil {
+		return sql.NullBool{Bool: *b, Valid: true}
+	}
+	return sql.NullBool{Valid: false}
+}
---
+++ converter/strtotype.go
@@ -0,0 +1,167 @@
+package converter
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Converter[T any] interface {
+	Convert(str string) (T, error)
+}
+
+type IntConverter struct{}
+
+func (c IntConverter) Convert(str string) (int, error) {
+	if str == "" {
+		return 0, fmt.Errorf("empty string cannot be converted to int")
+	}
+	intValue, err := strconv.Atoi(str)
+	if err != nil {
+		return 0, fmt.Errorf("not a valid integer: %v", err)
+	}
+	return intValue, nil
+}
+
+type FloatConverter struct{}
+
+func (c FloatConverter) Convert(str string) (float64, error) {
+	if str == "" {
+		return 0.0, fmt.Errorf("empty string cannot be converted to float")
+	}
+	floatValue, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return 0.0, fmt.Errorf("not a valid float: %v", err)
+	}
+	return floatValue, nil
+}
+
+type BoolConverter struct{}
+
+func (c BoolConverter) Convert(str string) (bool, error) {
+	if str == "" {
+		return false, fmt.Errorf("empty string cannot be converted to boolean")
+	}
+	boolVal, err := strconv.ParseBool(str)
+	if err != nil {
+		return false, fmt.Errorf("not a valid boolean: %v", err)
+	}
+	return boolVal, nil
+}
+
+type DateConverter struct{}
+
+func (c DateConverter) Convert(str string) (time.Time, error) {
+	if str == "" {
+		return time.Time{}, fmt.Errorf("empty string cannot be converted to date")
+	}
+	dateValue, err := time.Parse("2006-01-02", str)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("not a valid date: %v", err)
+	}
+	return dateValue, nil
+}
+
+type UnixTimeConverter struct{}
+
+func (c UnixTimeConverter) Convert(str string) (int, error) {
+
+	dateVal, err := ConvertString(str, DateConverter{})
+	if err != nil {
+		return -1, fmt.Errorf("not a valid integer: %v", err)
+	}
+
+	return int(misc.ToUnixTime(dateVal)), nil
+}
+
+type NullInt32Converter struct{}
+
+func (c NullInt32Converter) Convert(str string) (sql.NullInt32, error) {
+	if str == "" {
+		return sql.NullInt32{Valid: false}, nil
+	}
+	intValue, err := strconv.ParseInt(str, 10, 32)
+	if err != nil {
+		return sql.NullInt32{}, fmt.Errorf("not a valid integer: %v", err)
+	}
+	return sql.NullInt32{Int32: int32(intValue), Valid: true}, nil
+}
+
+type NullInt64Converter struct{}
+
+func (c NullInt64Converter) Convert(str string) (sql.NullInt64, error) {
+	if str == "" {
+		return sql.NullInt64{Valid: false}, nil
+	}
+	intValue, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return sql.NullInt64{}, fmt.Errorf("not a valid integer: %v", err)
+	}
+	return sql.NullInt64{Int64: intValue, Valid: true}, nil
+}
+
+type NullFloat64Converter struct{}
+
+func (c NullFloat64Converter) Convert(str string) (sql.NullFloat64, error) {
+	if str == "" {
+		return sql.NullFloat64{Valid: false}, nil
+	}
+	floatValue, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return sql.NullFloat64{}, fmt.Errorf("not a valid float: %v", err)
+	}
+	return sql.NullFloat64{Float64: floatValue, Valid: true}, nil
+}
+
+type NullBoolConverter struct{}
+
+func (c NullBoolConverter) Convert(str string) (sql.NullBool, error) {
+	if str == "" {
+		return sql.NullBool{Valid: false}, nil
+	}
+	boolVal, err := strconv.ParseBool(str)
+	if err != nil {
+		return sql.NullBool{}, fmt.Errorf("not a valid boolean: %v", err)
+	}
+	return sql.NullBool{Bool: boolVal, Valid: true}, nil
+}
+
+type NullStringConverter struct{}
+
+func (c NullStringConverter) Convert(str string) (sql.NullString, error) {
+	return sql.NullString{String: str, Valid: str != ""}, nil
+}
+
+type NullTimeConverter struct{}
+
+func (c NullTimeConverter) Convert(str string) (sql.NullTime, error) {
+	if str == "" {
+		return sql.NullTime{Valid: false}, nil
+	}
+	timeValue, err := time.Parse("2006-01-02", str)
+	if err != nil {
+		return sql.NullTime{}, fmt.Errorf("not a valid date: %v", err)
+	}
+	return sql.NullTime{Time: timeValue, Valid: true}, nil
+}
+
+type NullUnixTime64Converter struct{}
+
+func (c NullUnixTime64Converter) Convert(str string) (sql.NullInt64, error) {
+
+	dateVal, err := ConvertString(str, NullTimeConverter{})
+	if err != nil {
+		return sql.NullInt64{}, fmt.Errorf("not a valid integer: %v", err)
+	}
+
+	if !dateVal.Valid {
+		return sql.NullInt64{Valid: false}, nil
+	}
+
+	return sql.NullInt64{Int64: misc.ToUnixTime(dateVal.Time), Valid: true}, nil
+}
+
+func ConvertString[T any](str string, converter Converter[T]) (T, error) {
+	return converter.Convert(str)
+}
